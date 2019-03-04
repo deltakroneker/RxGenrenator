f92acf679663bad50881c526d0c106094c4fa9cf
deltakroneker <nikola.milic.ios@gmail.com>
2019-03-04 15:16:27 +0100

    Initial Commit

---
+++ genrenator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  genrenator
+//
+//  Created by Nikola Milic on 3/4/19.
+//  Copyright © 2019 Nikola Milic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
